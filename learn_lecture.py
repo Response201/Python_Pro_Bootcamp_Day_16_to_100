e71706d1a4f422792e55ac6fd60568547e79637e
jess <jessica.response201@gmail.com>
2025-12-09 13:59:48 +0100

    Day 16 - Done

---
+++ learn_lecture.py
@@ -0,0 +1,44 @@
+#from turtle import Turtle, Screen
+#class Car:
+#    def drive(self):      # ← METOD
+#        return f"{self.brand} kör!"
+#
+#car = Car()
+#car.brand = "Volvo"       # ← ATTRIBUT(brand)
+#
+## print(car.drive())        # anropar metoden
+#
+##Object     Class
+#timmy   =   Turtle()
+#
+##Object Attribut
+#timmy.shape("turtle")
+#timmy.color("OliveDrab4")
+#timmy.forward(100)
+#my_screen = Screen()
+#my_screen.canvheight
+#my_screen.exitonclick()
+#print(my_screen.canvheight)
+
+"""PyPi"""
+#Table
+
+#from prettytable import PrettyTable
+#table = PrettyTable()
+#
+#table.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
+#table.add_rows(
+#    [
+#        ["Adelaide", 1295, 1158259, 600.5],
+#        ["Brisbane", 5905, 1857594, 1146.4],
+#        ["Darwin", 112, 120900, 1714.7],
+#        ["Hobart", 1357, 205556, 619.5],
+#        ["Sydney", 2058, 4336374, 1214.8],
+#        ["Melbourne", 1566, 3806092, 646.9],
+#        ["Perth", 5386, 1554769, 869.4],
+#    ]
+#)
+#
+#table.border = False
+#table.align="l"
+#print(table)
