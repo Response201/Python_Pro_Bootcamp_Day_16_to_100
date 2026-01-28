5cc68cf535d4897c306c356d5164d899d8c99729
jess <jessica.response201@gmail.com>
2026-01-28 12:51:47 +0100

    30 - Project - Update NATO Alphabet - Error handling

--- Day 26/Project -  NATO Alphabet/main.py
+++ Day 26/Project -  NATO Alphabet/main.py
@@ -14,33 +14,56 @@ run=True
 
 # Frågar användaren efter ett ord att koda
 def question():
-    return textinput(f"NATO phonetic alphabet", "Type a word:" )
+
+    text_input = textinput(f"NATO phonetic alphabet", "Type a word:" )
+
+    # Om Cancel trycks → skicka tillbaks None
+    if text_input is None:
+        return None
+    # Om ordet är längre än 10 tecken eller innehåller 0 tecken -> fråga om nytt ord
+    if  len(text_input) >= 11 or  len(text_input) <= 0:
+          return ""
+
+    return text_input
 
 
 while run:
 
     text_input = question()
 
-    # Om ordet är 11 tecken eller längre, fråga om nytt ord
-    if text_input and len(text_input) >= 11:
-        text_input = question()
+    #  None -> avsluta programmet och loopen
+    if text_input is None:
+        run = False
+        screen.bye()
+        break
 
     if text_input:
+
         # Konvertera text_input till stora bokstäver, skapa dictionary med NATO-koder och lista med koder för varje bokstav
         text = list(text_input.upper())
         alpha_code = {row.letter: row.code for (index, row) in data.iterrows()}
-        alpha_word = [alpha_code[letter] for letter in text]
+
+        try:
+
+            alpha_word = [alpha_code[letter] for letter in text]
+
+        except KeyError:
+
+            print("Sorry, only letters in the alphabet please")
+
+        else:
+
+            # Skriv ut kodat ord
+            writer_text.text_code = alpha_word
+            writer_text.write_text()
+
+            # Vänta 2 sek, rensa text och ställ ny fråga
+            time.sleep(2)
+            writer_text.clear()
 
 
-        # Skriv ut kodat ord
-        writer_text.text_code = alpha_word
-        writer_text.write_text()
+else:
 
-        # Vänta 2 sek, rensa text och ställ ny fråga
-        time.sleep(2)
-        writer_text.clear()
-    else:
-        run =False
         screen.bye()
 
 screen.mainloop()
---
+++ Day 29-30/learn_lecture.py
@@ -0,0 +1,84 @@
+
+# File not found
+#with open("file.txt") as file:
+#        file.read()
+
+
+# Key error
+#a_dictionary = {"key": "value"}
+#vaule = a_dictionary["non_exist_key"]
+
+
+# Index error
+#list = [0,1,2]
+#item = list[3]
+
+# Type error
+# text = "abc"
+# print(text + 5)
+
+
+"""
+
+--- Catching exceptions ---
+
+try: Här skriver man den kod som kan orsaka ett fel.
+
+except: Här fångar man felet och bestämmer vad som ska hända om ett fel uppstår.
+
+else: Denna kod körs om inget fel inträffade i try.
+
+finally: Denna kod körs alltid, oavsett om ett fel uppstod eller inte.
+
+raise: skapa error -> Avbryter programflödet genom att utlösa ett specifikt exception,
+antingen ett eget eller ett befintligt, för att tala om att något är fel.
+
+
+
+
+
+try:
+    file = open("file.txt")
+    a_dictionary = {"key": "value"}
+    #print(a_dictionary["non_exist_key"])
+
+
+except FileNotFoundError:
+    print("File error")
+    file = open("file.txt", mode="w")
+    file.write("hej hej")
+
+except KeyError as error_message:
+    print("Key error", error_message)
+
+else:
+    content = file.read()
+    print(content)
+
+finally:
+    file.close()
+    raise TypeError("error som jag hittat på")
+
+
+
+
+age = -5
+
+if age < 0:
+    raise ValueError("Ålder kan inte vara negativ")
+
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
