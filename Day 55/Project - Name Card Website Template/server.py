c19196932e6ec77ddde2f2884d23ab44e619a26f
jess <jessica.response201@gmail.com>
2026-03-09 18:51:57 +0100

    55 - Project - Name Card Website Template + notes

---
+++ Day 55/Project - Name Card Website Template/server.py
@@ -0,0 +1,26 @@
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def create_html(func):
+    def wrapper(*args ):
+
+        func(*args)
+
+    return wrapper
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("404.html"), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
